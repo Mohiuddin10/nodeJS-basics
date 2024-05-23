6cd9105f58932481dec8688215181fb720c4d84a
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-05-23 21:46:00 +0600

    testing populer movie quotes

---
+++ Lecture 6- external package/index.js
@@ -0,0 +1,7 @@
+const movieQuotes = require("popular-movie-quotes");
+
+
+// console.log(movieQuotes.getRandomQuote());
+// console.log(movieQuotes.getAll());
+// console.log(movieQuotes.getQuotesByMovie("some like it hot"));
+console.log(movieQuotes.getQuoteByYear(2006, 2005));
